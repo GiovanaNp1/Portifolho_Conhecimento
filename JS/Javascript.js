560e3714bcd14cd2e50077bd4dbbdfff22c781da
Giovana Nascimento <giovana,np1@gmail.com>
2019-03-17 12:34:52 -0300

    second_commit

---
+++ JS/Javascript.js
@@ -0,0 +1,55 @@
+var Mes;
+
+function ConsultarMes() {
+    Mes = parseInt (document.fm_mes.numero_mes.value);
+    switch(Mes){
+        case 1:
+        window.alert ("Janeiro")
+        break;
+
+        case 2:
+        window.alert ("Feveriro")
+        break;
+
+        case 3:
+        window.alert ("Março")
+        break;
+
+        case 4:
+        window.alert ("Abril")
+        break;
+
+        case 5:
+        window.alert ("Maio")
+        break;
+
+        case 6:
+        window.alert ("Junho")
+        break;
+
+        case 7:
+        window.alert ("Julho")
+        break;
+
+        case 8:
+        window.alert ("Agosto")
+        break;
+
+        case 9:
+        window.alert ("Setembro")
+        break;
+
+        case 10:
+        window.alert ("Outubro")
+        break;
+
+        case 11:
+        window.alert ("Novembro")
+        break;
+
+        case 12:
+        window.alert ("Dezembro")
+        break;
+    }
+    
+}
